ffd2c4ff4fbd6b592fdf628521429347d80c7cef
Pierre Couprie <developer@pierrecouprie.fr>
2019-08-27 08:25:36 +0200

    Initial Commit

---
+++ MotusLabTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MotusLabTool
+//
+//  Created by Pierre Couprie on 27/08/2019.
+//  Copyright © 2019 Pierre Couprie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MotusLabTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MotusLabTool
+//
+//  Created by Pierre Couprie on 27/08/2019.
+//  Copyright © 2019 Pierre Couprie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
